b326f35bc70cbf7dd5b7c7325c1e7f18d0f8b59e
LGyorgy <l.gyorgy90@gmail.com>
2022-09-15 13:49:27 +0200

    Update APoD component to handle video content

--- src/App.js
+++ src/App.js
@@ -76,10 +76,18 @@ function APoD(props) {
     return <div className="apod">Loading...</div>
   }
 
+  let mediaContent = <p>No media</p>;
+
+  if (props.data.media_type === "video") {
+    mediaContent = <iframe className="apod-video" width="420" height="315" src={props.data.url}></iframe>;
+  } else {
+    mediaContent = <img className="apod-image" src={props.data.url} />
+  }
+
   return (
     <div className="apod">
       <p className="apod-date">{ new Date(props.data.date).toLocaleDateString(dateLocales, dateOptions) }</p>
-      <img className="apod-image" src={props.data.url} />
+      <div className="apod-media-content">{mediaContent}</div>
       <p className="apod-title">
         <strong>{props.data.title}</strong>
       </p>
