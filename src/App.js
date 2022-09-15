7e3790430e37574254f7c9a0b278d263afbe18fb
LGyorgy <l.gyorgy90@gmail.com>
2022-09-15 20:28:08 +0200

    Refactor gallery modal

--- src/App.js
+++ src/App.js
@@ -149,6 +149,22 @@ function Gallery(props) {
 }
 
 function GalleryItemCard(props) {
+  const [showModal, setShowModal] = useState(false);
+
+  const imageUrl = props.media_type === "video"
+    ? props.thumbnail_url
+    : props.url;
+
+  return <div className="gallery-item-card" onClick={() => {if (!showModal) setShowModal(true)}}>
+    <img className="gallery-image" width="auto" height="100px" src={imageUrl} alt={props.title}/>
+    <p className="gallery-image-title">{props.title}</p>
+    {showModal && (
+      <GalleryItemModal item={props} onClose={() => setShowModal(false)} show={showModal}/>
+    )}
+  </div>;
+}
+
+function GalleryItemModal(props) {
   const dateLocales = "en-US"
   const dateOptions = {
     year: 'numeric',
@@ -156,27 +172,23 @@ function GalleryItemCard(props) {
     day: 'numeric',
   };
 
-  const [showModal, setShowModal] = useState(false);
-
-  let imageUrl = props.media_type === "video"
-    ? props.thumbnail_url
-    : props.url;
+  const imageUrl = props.item.media_type === "video"
+    ? props.item.thumbnail_url
+    : props.item.url;
 
   const modalBody = (
     <div>
       <div className="gallery-modal-image">
         <img className="gallery-image" width="auto" height="200px" src={imageUrl} alt={props.title}/>
       </div>
-      <p><strong>Date:</strong> {new Date(props.date).toLocaleDateString(dateLocales, dateOptions)}</p>
-      <p><strong>Explanation:</strong> {props.explanation}</p>
+      <p><strong>Date:</strong> {new Date(props.item.date).toLocaleDateString(dateLocales, dateOptions)}</p>
+      <p><strong>Explanation:</strong> {props.item.explanation}</p>
     </div>
   );
 
-  return <div className="gallery-item-card" onClick={() => {if (!showModal) setShowModal(true)}}>
-    <img className="gallery-image" width="auto" height="100px" src={imageUrl} alt={props.title}/>
-    <p className="gallery-image-title">{props.title}</p>
-    <Modal title={props.title} body={modalBody} onClose={() => setShowModal(false)} show={showModal}/>
-  </div>;
+  return (
+    <Modal title={props.item.title} body={modalBody} show={props.show} onClose={props.onClose}/>
+  );
 }
 
 function useDataFetching(defaultUrl) {
