475c8f33f6f66c439137da0c105df9dfc68363a7
LGyorgy <l.gyorgy90@gmail.com>
2022-09-15 20:14:33 +0200

    Fill gallery modals with content

--- src/App.js
+++ src/App.js
@@ -149,16 +149,31 @@ function Gallery(props) {
 }
 
 function GalleryItemCard(props) {
+  const dateLocales = "en-US"
+  const dateOptions = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  };
+
   const [showModal, setShowModal] = useState(false);
 
   let imageUrl = props.media_type === "video"
     ? props.thumbnail_url
     : props.url;
 
+  const modalBody = (
+    <div>
+      <img className="gallery-image" width="auto" height="200px" src={imageUrl} alt={props.title}/>
+      <p><strong>Date:</strong> {new Date(props.date).toLocaleDateString(dateLocales, dateOptions)}</p>
+      <p><strong>Explanation:</strong> {props.explanation}</p>
+    </div>
+  );
+
   return <div className="gallery-item-card" onClick={() => {if (!showModal) setShowModal(true)}}>
     <img className="gallery-image" width="auto" height="100px" src={imageUrl} alt={props.title}/>
     <p className="gallery-image-title">{props.title}</p>
-    <Modal onClose={() => setShowModal(false)} show={showModal}/>
+    <Modal title={props.title} body={modalBody} onClose={() => setShowModal(false)} show={showModal}/>
   </div>;
 }
 
@@ -203,10 +218,10 @@ function Modal(props) {
     <div className="modal">
       <div className="modal-content">
         <div className="modal-header">
-          <h4 className="modal-title">Modal title</h4>
+          <h4 className="modal-title">{props.title}</h4>
         </div>
         <div className="modal-body">
-          Modal content
+          {props.body}
         </div>
         <div className="modal-footer">
           <button className="button" onClick={props.onClose}>Close</button>
