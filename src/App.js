9281ae37b4ec81ede49fbe76c97e40b283eecbcd
LGyorgy <l.gyorgy90@gmail.com>
2022-09-15 21:02:07 +0200

    Refactor APoDApp into separate file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState } from "react";
+import { useDataFetching, Modal } from "./Utils";
+import APoDApp from "./APoDApp";
 import "./App.css";
 export default App;
 
@@ -25,7 +27,7 @@ function App() {
       <p><PageLink text="Gallery" onClick={() => switchPage(pages.gallery)} /></p>
     </div>
   )
-  
+
   return (
     <div>
       {pageLinks}
@@ -33,6 +35,7 @@ function App() {
     </div>
   )
 }
+
 
 function PageLink(props) {
   return (
@@ -42,91 +45,14 @@ function PageLink(props) {
   );
 }
 
-function APoDApp() {
-  const apiUrl = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
-  const [date, setDate] = useState((new Date()).toISOString().substring(0,10));
-  const getFullUrl = useCallback(() => {
-    return apiUrl + `&date=${date}`;
-  }, [apiUrl, date]);
-
-  const [data, loading, error, setUrl] = useDataFetching(getFullUrl());
-
-  useEffect(() => {
-    setUrl(getFullUrl());
-  }, [setUrl, getFullUrl])
-
-  return (
-    <div>
-      <h1>Astronomy Picture of the Day</h1>
-      <DatePicker date={date} onChange={(date) => {
-          setDate(date);
-        }}
-      />
-      {loading && <div>Loading...</div>}
-      {error && (
-        <div>There was an error while fetching data: {error}</div>
-      )}
-      {data && (
-        <APoD data={data}/>
-      )}
-    </div>
-  );
-}
-
-function DatePicker(props) {
-  const handleChange = (e) => {
-    props.onChange(e.target.value);
-  }
-
-  return (
-    <form>
-      <input onChange={handleChange} type="date" name="date" value={props.date}/>
-    </form>
-  );
-}
-
-function APoD(props) {
-  const dateLocales = "en-US"
-  const dateOptions = {
-    weekday: 'long',
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  };
-
-  if (props.data === null) {
-    return <div className="apod">Loading...</div>
-  }
-
-  let mediaContent = <p>No media</p>;
-
-  if (props.data.media_type === "video") {
-    mediaContent = <iframe className="apod-video" width="420" height="315" title={props.data.title} src={props.data.url}></iframe>;
-  } else {
-    mediaContent = <img className="apod-image" alt={props.data.title} src={props.data.url} />
-  }
-
-  return (
-    <div className="apod">
-      <p className="apod-date">{ new Date(props.data.date).toLocaleDateString(dateLocales, dateOptions) }</p>
-      <div className="apod-media-content">{mediaContent}</div>
-      <p className="apod-title">
-        <strong>{props.data.title}</strong>
-      </p>
-      <p className="apod-explanation">
-        <strong>Explanation:</strong> {props.data.explanation}
-      </p>
-    </div>
-  );
-}
 
 function GalleryApp() {
   const apiUrl = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&thumbs=True";
 
   const todayDate = new Date()
-  const todayStr = todayDate.toISOString().substring(0,10);
+  const todayStr = todayDate.toISOString().substring(0, 10);
   const fourWeeksAgoDate = new Date(todayDate.getFullYear(), todayDate.getMonth(), todayDate.getDate() - (7 * 4));
-  const fourWeeksAgoStr = fourWeeksAgoDate.toISOString().substring(0,10);
+  const fourWeeksAgoStr = fourWeeksAgoDate.toISOString().substring(0, 10);
 
   const [data, loading, error, setUrl] = useDataFetching(apiUrl + `&end_date=${todayStr}&start_date=${fourWeeksAgoStr}`);
 
@@ -144,14 +70,16 @@ function GalleryApp() {
   );
 }
 
+
 function Gallery(props) {
   return (
-      <div className="gallery">{props.data.map( (item) => {
-        return <GalleryItemCard key={item.date} {...item} />
-      })}
-      </div>
+    <div className="gallery">{props.data.map((item) => {
+      return <GalleryItemCard key={item.date} {...item} />
+    })}
+    </div>
   );
 }
+
 
 function GalleryItemCard(props) {
   const [showModal, setShowModal] = useState(false);
@@ -160,14 +88,15 @@ function GalleryItemCard(props) {
     ? props.thumbnail_url
     : props.url;
 
-  return <div className="gallery-item-card" onClick={() => {if (!showModal) setShowModal(true)}}>
-    <img className="gallery-image" width="auto" height="100px" src={imageUrl} alt={props.title}/>
+  return <div className="gallery-item-card" onClick={() => { if (!showModal) setShowModal(true) }}>
+    <img className="gallery-image" width="auto" height="100px" src={imageUrl} alt={props.title} />
     <p className="gallery-image-title">{props.title}</p>
     {showModal && (
-      <GalleryItemModal item={props} onClose={() => setShowModal(false)} show={showModal}/>
+      <GalleryItemModal item={props} onClose={() => setShowModal(false)} show={showModal} />
     )}
   </div>;
 }
+
 
 function GalleryItemModal(props) {
   const dateLocales = "en-US"
@@ -184,7 +113,7 @@ function GalleryItemModal(props) {
   const modalBody = (
     <div>
       <div className="gallery-modal-image">
-        <img className="gallery-image" width="auto" height="200px" src={imageUrl} alt={props.title}/>
+        <img className="gallery-image" width="auto" height="200px" src={imageUrl} alt={props.title} />
       </div>
       <p><strong>Date:</strong> {new Date(props.item.date).toLocaleDateString(dateLocales, dateOptions)}</p>
       <p><strong>Explanation:</strong> {props.item.explanation}</p>
@@ -192,60 +121,6 @@ function GalleryItemModal(props) {
   );
 
   return (
-    <Modal title={props.item.title} body={modalBody} show={props.show} onClose={props.onClose}/>
-  );
-}
-
-function useDataFetching(defaultUrl) {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [url, setUrl] = useState(defaultUrl);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setData(null);
-      try {
-        const response = await fetch(url);
-        if (!response.ok) {
-          throw new Error(
-            `HTTP error: The status is ${response.status}`
-          );
-        }
-        let data = await response.json();
-        setData(data);
-        setError(null);
-      } catch(err) {
-        setError(err.message);
-        setData(null)
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchData();
-  }, [url]);
-
-  return [data, loading, error, setUrl];
-}
-
-function Modal(props) {
-  if (!props.show) {
-    return null;
-  }
-
-  return (
-    <div className="modal">
-      <div className="modal-content">
-        <div className="modal-header">
-          <h4 className="modal-title">{props.title}</h4>
-        </div>
-        <div className="modal-body">
-          {props.body}
-        </div>
-        <div className="modal-footer">
-          <button className="button" onClick={props.onClose}>Close</button>
-        </div>
-      </div>
-    </div>
+    <Modal title={props.item.title} body={modalBody} show={props.show} onClose={props.onClose} />
   );
 }
---
+++ src/Utils.js
@@ -0,0 +1,69 @@
+import { useState, useEffect, useCallback } from "react";
+
+export function useDataFetching(defaultUrl) {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [url, setUrl] = useState(defaultUrl);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setData(null);
+            try {
+                const response = await fetch(url);
+                if (!response.ok) {
+                    throw new Error(
+                        `HTTP error: The status is ${response.status}`
+                    );
+                }
+                let data = await response.json();
+                setData(data);
+                setError(null);
+            } catch (err) {
+                setError(err.message);
+                setData(null)
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchData();
+    }, [url]);
+
+    return [data, loading, error, setUrl];
+}
+
+
+export function Modal(props) {
+    if (!props.show) {
+        return null;
+    }
+
+    return (
+        <div className="modal">
+            <div className="modal-content">
+                <div className="modal-header">
+                    <h4 className="modal-title">{props.title}</h4>
+                </div>
+                <div className="modal-body">
+                    {props.body}
+                </div>
+                <div className="modal-footer">
+                    <button className="button" onClick={props.onClose}>Close</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+
+export function DatePicker(props) {
+    const handleChange = (e) => {
+        props.onChange(e.target.value);
+    }
+
+    return (
+        <form>
+            <input onChange={handleChange} type="date" name="date" value={props.date} />
+        </form>
+    );
+}
