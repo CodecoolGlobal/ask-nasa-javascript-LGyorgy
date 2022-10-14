f1cdeb492ff20b2c412ddf253c5bd97b1a9a8b56
LGyorgy <l.gyorgy90@gmail.com>
2022-10-14 10:08:48 +0200

    Unpack props parameter

--- src/components/apod/APoDComponent.js
+++ src/components/apod/APoDComponent.js
@@ -1,4 +1,4 @@
-const APoDComponent = (props) => {
+const APoDComponent = ({ data }) => {
     const dateLocales = "en-US"
     const dateOptions = {
         weekday: 'long',
@@ -7,27 +7,27 @@ const APoDComponent = (props) => {
         day: 'numeric',
     };
 
-    if (props.data === null) {
+    if (data === null) {
         return <div className="apod">Loading...</div>
     }
 
     let mediaContent = <p>No media</p>;
 
-    if (props.data.media_type === "video") {
-        mediaContent = <iframe className="apod-video" width="420" height="315" title={props.data.title} src={props.data.url}></iframe>;
+    if (data.media_type === "video") {
+        mediaContent = <iframe className="apod-video" width="420" height="315" title={data.title} src={data.url}></iframe>;
     } else {
-        mediaContent = <img className="apod-image" alt={props.data.title} src={props.data.url} />
+        mediaContent = <img className="apod-image" alt={data.title} src={data.url} />
     }
 
     return (
         <div className="apod">
-            <p className="apod-date">{new Date(props.data.date).toLocaleDateString(dateLocales, dateOptions)}</p>
+            <p className="apod-date">{new Date(data.date).toLocaleDateString(dateLocales, dateOptions)}</p>
             <div className="apod-media-content">{mediaContent}</div>
             <p className="apod-title">
-                <strong>{props.data.title}</strong>
+                <strong>{data.title}</strong>
             </p>
             <p className="apod-explanation">
-                <strong>Explanation:</strong> {props.data.explanation}
+                <strong>Explanation:</strong> {data.explanation}
             </p>
         </div>
     );
